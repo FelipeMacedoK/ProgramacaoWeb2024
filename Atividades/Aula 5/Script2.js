e5c500398338e9ea4aa47d769f057d7f6428de39
FelipeMacedoK <felipe.macedo.0507@unidavi.edu.br>
2024-08-26 21:12:21 -0300

    Aula 26/08

--- Atividades/Aula 5/Script2.js
+++ Atividades/Aula 5/Script2.js
@@ -1,7 +1,31 @@
 function definirNotas() {
-    for (let i = 0; i == 60; i++) {
-        var teste = document.getElementsByTagName("td")[i].value ? "" : document.getElementsByTagName("td").value = Math.floor(5 + Math.random() * 5);
-        console.log (teste);
-        return teste;
+    var cell = document.getElementsByTagName("td")
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].innerText == "" ? cell[i].innerText = parseFloat(5 + Math.random() * 5).toFixed(1) : "";
     }
+    document.getElementById("definir").style.display = "none"; 
+    document.getElementById("linha").style.display = "inline"; 
+    document.getElementById("coluna").style.display = "inline";
+    console.log (cell);
+}
+
+function mediaLinha() {
+    var cell = document.getElementsByTagName("td");
+    var html = '<td><strong>Média</strong></td><td>';
+    for (let i = 2; i < 10; i++) {
+        var total = 0;
+        var media = 0;
+        for (let j = i; j < cell.length; j = j+10) {
+            total += parseFloat(cell.innerHTML);       
+        }
+        media = total/6;
+        html += media + '</td><td>'
+    }
+    document.createElement('tr').innerHTML = html;
+    tabela.appendChild(row);
+    document.getElementById("linha").style.display = "none";
+}
+
+function mediaColuna() {
+    document.getElementById("coluna").style.display = "none"; 
 }
