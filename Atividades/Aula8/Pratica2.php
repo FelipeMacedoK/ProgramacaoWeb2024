e886315b6dd32eb6bf278d6185423a60fe599992
FelipeMacedoK <felipe.macedo.0507@gmail.com>
2024-09-13 19:30:25 -0300

    Renomeação das Pastas

---
+++ Atividades/Aula8/Pratica2.php
@@ -0,0 +1,11 @@
+<?php
+
+//Prática 2
+    define ('nome', 'Felipe');
+    define ('sobrenome', 'Macedo');
+
+    $nomeCompleto = nome . ' ' . sobrenome;
+
+    echo "Meu nome completo é $nomeCompleto";
+
+?>
